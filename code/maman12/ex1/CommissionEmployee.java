81ca7a5341fb9464c33a06fcc223e467a77edd71
Dorel Masasa <masasaDorel@gmail.com>
2021-12-01 14:07:11 +0200

    ex1 input files

---
+++ code/maman12/ex1/CommissionEmployee.java
@@ -0,0 +1,58 @@
+package code.maman12.ex1;
+
+
+public class CommissionEmployee extends Employee {
+    private double grossSales;
+    private double commissionRate;
+
+    public CommissionEmployee(String firstName, String lastName, String socialSecurityNumber,
+                              double grossSales, double commissionRate) {
+        super(firstName, lastName, socialSecurityNumber);
+
+        if (commissionRate <= 0 || commissionRate >= 1) {
+            throw new IllegalArgumentException("Commission rate must be > 0 and < 1");
+        }
+
+        if (grossSales < 0) {
+            throw new IllegalArgumentException("Gross sales must be >= 0");
+        }
+
+        this.grossSales = grossSales;
+        this.commissionRate = commissionRate;
+    }
+
+    public void setGrossSales(double grossSales) {
+        if (grossSales < 0) {
+            throw new IllegalArgumentException("Gross sales must be >= 0");
+        }
+
+        this.grossSales = grossSales;
+    }
+
+    public void setCommissionRate(double commissionRate) {
+        if (commissionRate <= 0 || commissionRate >= 1) {
+            throw new IllegalArgumentException("Commission rate must be > 0 and < 1");
+        }
+
+        this.commissionRate = commissionRate;
+    }
+
+    public double getGrossSales() {
+        return grossSales;
+    }
+
+    public double getCommissionRate() {
+        return commissionRate;
+    }
+
+    @Override
+    public double earnings() {
+        return getCommissionRate() * getGrossSales();
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Commission employee: %s%n%s: $%.2f; %s: %.2f",
+                super.toString(), "Gross sales", getGrossSales(), "Commission rate", getCommissionRate());
+    }
+}
---
+++ code/maman12/ex1/HourlyEmployee.java
@@ -0,0 +1,63 @@
+package code.maman12.ex1;
+
+
+public class HourlyEmployee extends Employee {
+    private double wage;
+    private double hours;
+
+    public HourlyEmployee(String firstName, String lastName, String socialSecurityNumber,
+                          double wage, double hours) {
+        super(firstName, lastName, socialSecurityNumber);
+
+        if (wage < 0) {
+            throw new IllegalArgumentException("Hourly wage must be >= 0");
+        }
+
+        if ((hours < 0) || (hours > 168)) {
+            throw new IllegalArgumentException("Hours worked must be >= 0 and <= 168");
+        }
+
+        this.wage = wage;
+        this.hours = hours;
+    }
+
+    public void setWage(double wage) {
+        if (wage < 0) {
+            throw new IllegalArgumentException("Hourly wage must be >= 0");
+        }
+
+        this.wage = wage;
+    }
+
+    public void setHours(double hours) {
+        if ((hours < 0) || (hours > 168)) {
+            throw new IllegalArgumentException("Hours worked must be >= 0 and <= 168");
+        }
+
+        this.hours = hours;
+    }
+
+    public double getWage() {
+        return wage;
+    }
+
+    public double getHours() {
+        return hours;
+    }
+
+    @Override
+    public double earnings() {
+        if (getHours() <= 40){
+            return getWage() * getHours();
+        }
+        else {
+            return 40 * getWage() + (getHours() - 40) * getWage() * 1.5;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Hourly employee: %s%n%s : $%.2f; %s: %,.2f",
+                super.toString(), "Hourly wage", getWage(), "hours worked", getHours());
+    }
+}
