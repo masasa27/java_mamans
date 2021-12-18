573549be4a817261f4db9a2965d5e0b5aa5cfd7f
Dorel Masasa <masasaDorel@gmail.com>
2021-12-18 14:48:53 +0200

    basic ui for question founded

--- code/maman13/FileReader.java
+++ code/maman13/FileReader.java
@@ -3,37 +3,41 @@ package code.maman13;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class FileReader {
+    public List<Question> questions = new ArrayList<Question>();
+    public String absFilePath;
 
     public FileReader(String filename) throws IOException {
         int rowIndex = 0;
-        try {
-            // TODO: abspath and not heartcoded path
+        // getting file path
+        String path = new File("./code/maman13").getAbsolutePath();
+        File absPathFile = new File(path, filename);
+        this.absFilePath = absPathFile.getPath();
+    }
 
-            // getting file path
-            String path = new File("./code/maman13").getAbsolutePath();
-            File absPathFile = new File(path, filename);
-            String absPath = absPathFile.getPath();
+    public List<Question> getQuestions() {
+        int rowIndex = 0;
+        try {
 
             // reading file
-            File myObj = new File(absPath);
+            File myObj = new File(this.absFilePath);
             Scanner myReader = new Scanner(myObj);
             while (myReader.hasNextLine()) {
                 String data = myReader.nextLine();
-                
-                if  (rowIndex % 5 == 0)
-                {
+
+                if (rowIndex % 5 == 0) {
                     Question q = new Question(data);
-                }
-                else if (rowIndex % 5 == 1)
-                {
+                    this.questions.add(q);
+                } else if (rowIndex % 5 == 1) {
                     Answer answer = new Answer(data, true);
-                }
-                else
-                {
+                    this.questions.get(this.questions.size() - 1).addAnswer(answer);
+                } else {
                     Answer answer = new Answer(data, false);
+                    this.questions.get(this.questions.size() - 1).addAnswer(answer);
                 }
 
                 System.out.println(rowIndex);
@@ -45,8 +49,6 @@ public class FileReader {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
-
-        
+        return this.questions;
     }
-
 }
--- code/maman13/Questionary.java
+++ code/maman13/Questionary.java
@@ -2,13 +2,16 @@ package code.maman13;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 public class Questionary {
     public static void main(String[] args) throws IOException
     {
-        System.out.println("hello world");
+        
 
         FileReader reader = new FileReader("questions.txt");
+        List<Question> questions = reader.getQuestions();
+        System.out.println("hello world");
     }
     
 }
